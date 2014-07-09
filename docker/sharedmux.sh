6984d7de98aeab44129d46d70b3dd1e5b036490a
Whi Morriss <whit.morriss@canonical.com>
2014-07-09 10:53:22 -0500

    deletes

--- docker/sharedmux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-tmux -S /tmp/pair_socket new -ds shared
-chown ubuntu:ubuntu /tmp/pair_socket
-
-/usr/sbin/sshd -D
--- docker/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo docker stop test_sshd && sudo docker rm test_sshd
-set -e
-sudo docker build --rm -t cfpbj ./
-sudo docker run -d -P --name test_sshd cfpbj
-sudo docker port test_sshd 22
