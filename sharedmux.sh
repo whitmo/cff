7046c8550be49c7b72c80faa7c2786e7c0e941ef
Whi Morriss <whit.morriss@canonical.com>
2014-07-09 16:06:16 -0500

    explicit output declaration for ssh import

--- sharedmux.sh
+++ sharedmux.sh
@@ -5,9 +5,9 @@ set -e
 tmux -S /tmp/pair_socket new -ds shared
 chown ubuntu:ubuntu /tmp/pair_socket
 
-/usr/sbin/sshd -D
-
 echo "$SSH_USERIDS" >> /home/ubuntu/wat.txt
 if [ -n "$SSH_USERIDS" ]; then
-    sudo --user=ubuntu ssh-import-id "$SSH_USERIDS"
+    ssh-import-id -o /home/ubuntu/.ssh/authorized_keys "$SSH_USERIDS"
 fi
+
+/usr/sbin/sshd -D
