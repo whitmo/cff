634300acd56b9e99e40849565c43cb8391c336b2
Whi Morriss <whit.morriss@canonical.com>
2014-07-09 16:45:10 -0500

    get'r done  - print out a working ssh command from test.sh for hand testing  - make vars interpolate nicely in launcher

--- sharedmux.sh
+++ sharedmux.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 set -e
 
 tmux -S /tmp/pair_socket new -ds shared
 chown ubuntu:ubuntu /tmp/pair_socket
 
-echo "$SSH_USERIDS" >> /home/ubuntu/wat.txt
 if [ -n "$SSH_USERIDS" ]; then
-    ssh-import-id -o /home/ubuntu/.ssh/authorized_keys "$SSH_USERIDS"
+    CMD="ssh-import-id -o /home/ubuntu/.ssh/authorized_keys $SSH_USERIDS"
+    $CMD
 fi
 
 /usr/sbin/sshd -D
--- test.sh
+++ test.sh
@@ -2,5 +2,7 @@
 sudo docker stop test_sshd && sudo docker rm test_sshd
 set -e
 sudo docker build --rm -t cfpbj ./
-sudo docker run -d -P --name test_sshd -e "SSH_USERIDS='gh:whitmo'" cfpbj
-sudo docker port test_sshd 22
+sudo docker run -dP --name test_sshd -e "SSH_USERIDS=gh:whitmo lp:johnsca" cfpbj
+
+CMD=$(sudo docker port test_sshd 22 | sed "s/^[\.0]*:\([0-9]*\)/ssh ubuntu@localhost -p \1 -t tmux -S \/tmp\/pair_socket a -t shared/")
+echo $CMD
